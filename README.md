d7e5d85c833ad4ca9ab76174b3e1a96b5f877a6f
Milton Honji <factorysolutionIT@outlook.com>
2021-01-25 22:55:24 -0300

    Primeira parte do desafio Unica c# - Classes e Métodos.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Desafio-CSharp-Unica
-Desafio CSharp Unica
+# treinamento-poo-appbanco
+:computer: :blue_book: 
+
+Este é o repositório foi criado para turma Carreira Única do treinamento Programação Orientado a Objetos. Este é o desafio que vocês terão que concluir. Leia as informações abaixo:
+ 
+ <h4 align="center"> 
+	🚧  Projeto 🚀 Em construção...  🚧
+ </h4>
+ 
+ ### Siga as instruções do roteiro :page_facing_up:
+ 
+## Criação da classe ContaCorrente
+##### 1 - Crie a classe ContaCorrente é a nossa entidade e você deverá cria-la dentro da pasta Model, com os seguintes atributos: 
+- Titular string
+- Agencia int
+- Numero int
+- Saldo double
+
+## Criação da classe ContaCorrenteService
+##### 2 - Crie a classe ContaCorrente é a nossa de serviço onde contém nossas regras de negócio e você deverá cria-la dentro da pasta Service, e nela deve conter: 
+- Método OperacaoSaque do tipo void (sem retorno):heavy_check_mark:
+- Método OperacaoDeposito do tipo void (sem retorno) :heavy_check_mark:
+- Método OperacaoTransferencia do tipo void (sem retorno) :heavy_check_mark:
+
+
+## Criação dos métodos privados
+##### 3 - Crie os métodos privados abaixo: 
+- Método Sacar() deve ser privado e recebe um double valor e double saldo, deverá fazer o cálculo para sacar o valor e exibir mensagem ao usuário " Não foi possível concluir a transação. Seu saldo é inferior ao valor do saque!" caso o usuário informe um valor de saque maior do que tem em sua conta.
+- Método Depositar() deve ser privado e recebe um double valor e double saldo, a funcionaldiade é adicionar o valor na conta do usuário. 
+- Método Transferir() deve ser privado e recebe um double valor, double saldo e uma conta corrente destino, você deverá instanciar dois objetos conta corrente 1 conta corrente de origem e uma conta corrente de destino. Esse método deverá exibir a mensagem: "ão foi possível concluir a transação. Seu saldo é inferior ao valor que deseja transferir!" caso o saldo seja menior que o valor da transferência.
+
+## Criação dos Métodos
+##### 4 - Os métodos acima foram criados mas é preciso implementar sua funcionalidade, conforme descrito abaixo:
+- OperacaoSaque =>  deve chamar o método Saque e realizar as operações de entrada e saída das informações para o usuário
+- OperacaoDeposito => deve chamar o método Depósito e realizar as operações de entrada e saída das informações para o usuário
+- OperacaoTransferencia => deve chamar o método Transferir e realizar as operações de entrada e saída das ifnormações para o usuário
+
+
+É aconselhável realizar a consulta e utilizar como referência o projeto do repositório (https://github.com/JessicaNathany/treinamento-poo) que foi utilizado nos estudos em aula.Nele vocês podem obter a sintaxe do c#, código para exibir mensagem para o usuário Console.WriteLine() e instância de objeto.
+
+
+Um desafio a mais para você colocar mais em prática os conceitos. Seguin como base a ContaCorrente, crie uma entidade ContaInvestimento e ContaInvestimentoService e faça as mesmas ações ou até mais ações da ContaCorrente, você pode utilizar a Herança e criar uma superclasse para as subclasses ContaCorrente e ContaInvestimento.
---
+++ treinamento-poo/Service/ContaCorrenteService.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using treinamento_poo.Model;
+
+namespace treinamento_poo.Service
+{
+    /// <summary>
+    /// Nesta classe vamos manter todas as regras de negócio da nossa pequena aplicação
+    /// </summary>
+    public class ContaCorrenteService
+    {
+        #region Metodos Públicos
+
+        public void OperacaoSaque()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Digite seu nome: " + "\n");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Informe um valor que deseja sacar: " + "\n");
+            var valor = Console.ReadLine();
+
+            var valorConvertido = Convert.ToDouble(valor);
+
+            //Informações da contaCorrente
+            var contaCorrente = new ContaCorrente(){
+                Titular = nome,
+                Agencia = 2501,
+                Numero = 202101,
+                Saldo = 1500.00
+            };
+
+            //Double
+            // Implemente a ação sacar aqui e exiba as informações para o usuário...
+            var saque = Sacar(valorConvertido, contaCorrente.Saldo);
+
+            //Frase
+            Console.WriteLine();
+            Console.WriteLine($"O {contaCorrente.Titular} " + $"fez o saque de: { valorConvertido} " + "\n" + 
+                $"pois, o seu saldo agora é de: {saque} reais.");
+
+            Console.ReadKey();
+        }
+
+        public void OperacaoDeposito()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Digite seu nome: " + "\n");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Informe um valor que deseja depositar: " + "\n");
+            var valor = Console.ReadLine();
+
+            var valorConvertido = Convert.ToDouble(valor);
+
+            //Informações da contaCorrente
+            var contaCorrente = new ContaCorrente()
+            {
+                Titular = nome,
+                Agencia = 2501,
+                Numero = 202101,
+                Saldo = 1500.00
+            };
+
+            // implemente a ação depositar aqui e exiba as informações para o usuário...
+            var deposito = Depositar(valorConvertido, contaCorrente.Saldo);
+
+            //Frase
+            Console.WriteLine();
+            Console.WriteLine($"Foi efetuado um deposito no valor de: {valorConvertido} reais. " + $"Agora o {nome} " + $"está com R$ {deposito} reais.");
+
+            Console.ReadKey();
+        }
+
+        public void OperacaoTransferencia()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Digite seu nome: " + "\n");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Informe um valor que deseja transferir: " + "\n");
+            var valor = Console.ReadLine();
+            var valorConvertido = Convert.ToDouble(valor);
+
+            //Informações da contaCorrente
+            var contaCorrente = new ContaCorrente()
+            {
+                Titular = nome,
+                Agencia = 2501,
+                Numero = 202101,
+                Saldo = 1500.00
+            };
+
+            var contaDestino = new ContaCorrente()
+            {
+                Titular = "Jessica",
+                Agencia = 2601,
+                Numero = 202101,
+                Saldo = 34.00
+            };
+
+            // implemente a ação transferir aqui e exiba as informações para o usuário
+            var transferencia = Transferir(valorConvertido, contaCorrente.Saldo, contaDestino);
+
+            //Tirando a prova real.
+            //Console.WriteLine();
+            //Console.WriteLine($"Conforme feito o deposito, agora (ela/ele): {contaDestino.Titular} está com o saldo de: R$ {contaDestino.Saldo} reais.");
+
+            Console.ReadKey();
+        }
+        #endregion Metodos Públicos
+
+        // criar os métodos Sacar, Depositar e Transferir como private
+        #region Métodos Privados
+
+        private double Sacar(double valor, double saldo)
+        { 
+            if (saldo < valor)
+            {
+                Console.WriteLine("Não foi possível concluir a tansação. Seu saldo é inferiro ao valor do saque");
+            }
+            else
+            {
+                //O Saldo irá subtrair com o valor digitado (em outro método)
+                saldo -= valor;
+                return saldo;
+            }
+
+            return saldo;
+        }
+
+        private double Depositar(double valor, double saldo)
+        {
+            saldo += valor;
+            return saldo;
+        }
+
+        private bool Transferir(double valor, double saldo, ContaCorrente contaDestino)
+        {
+            if(saldo < valor)
+            {
+                Console.WriteLine("Não foi possível concluir a transação. Seu saldo é inferior ao valor que deseja transferir");
+            }
+            else
+            {
+                //O saldo da conta origem irá subtrair o valor
+                saldo -= valor;
+                //Enfiim a conta destino receberá o valor digitado
+                contaDestino.Saldo += valor;
+                Console.WriteLine($"Transferencia realizada com sucesso!. O seu saldo agora é de: R$ {saldo} reais.");
+
+
+            }
+            return true;
+        }
+
+        #endregion Métodos Privados
+    }
+}
